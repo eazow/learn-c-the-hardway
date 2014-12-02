8e9ec8ff51b019764105c621504245a3c1b67eca
hhmhywzr <eazow@163.com>
2014-12-02 20:46:12 +0800

    update ex17

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <asert.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
-#define MAX_DATA =512
+#define MAX_DATA 512
 #define MAX_ROWS 100
 
 struct Address {
@@ -12,16 +12,16 @@ struct Address {
 	int set;
 	char name[MAX_DATA];
 	char email[MAX_DATA];
-}
+};
 
 struct Database {
 	struct Address rows[MAX_ROWS];
-}
+};
 
 struct Connection {
 	FILE *file;
 	struct Database *db;
-}
+};
 
 void die(const char *message)
 {
@@ -42,7 +42,7 @@ void Address_print(struct Address *addr)
 void Database_load(struct Connection *conn)
 {
 	int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
-	if(rc != 2) die("Failed to load database.");
+	if(rc != 1) die("Failed to load database.");
 }
 
 struct Connection * Database_open(const char *filename, char mode)
@@ -101,17 +101,100 @@ void Database_create(struct Connection *conn)
 	}
 }
 	
+void Database_set(struct Connection *conn, int id, const char *name, const char *email)
+{
+	struct Address *addr = &conn->db->rows[id];
+	if(addr->set) die("Already set, delete it first");
 
+	addr->set = 1;
+	//WARNING: bug, read the "How To Break It" and fix this
+	char *res = strncpy(addr->name, name, MAX_DATA);
+	//demonstrate the strncpy bug
+	if(!res) die("Name copy failed");
 
+	res = strncpy(addr->email, email, MAX_DATA);
+	if(!res) die("Email copy failed");
+}
 
+void Database_get(struct Connection *conn, int id)
+{
+	struct Address *addr = &conn->db->rows[id];
 
+	if(addr->set) {
+		Address_print(addr);
+	}
+	else {
+		die("ID is not set");
+	}
+}
 
+void Database_delete(struct Connection *conn, int id)
+{
+	struct Address addr = {.id=id, .set=0};
+	conn->db->rows[id] = addr;
+}
 
+void Database_list(struct Connection *conn)
+{
+	int i = 0;
+	struct Database *db = conn->db;
 
+	for(i=0; i<MAX_ROWS; i++) {
+		struct Address *cur = &db->rows[i];
 
+		if(cur->set) {
+			Address_print(cur);
+		}
+	}
+}
 
+int main(int argc, char *argv[])
+{
+	if(argc < 3) die("USAGE: ex17 <dbfile> <action> [action params]");
 
+	char *filename = argv[1];
+	char action = argv[2][0];
+	struct Connection *conn = Database_open(filename, action);
+	int id = 0;
 
+	if(argc > 3) id = atoi(argv[3]);
+	if(id >= MAX_ROWS) die("There's not that many records.");
 
+	switch(action) {
+		case 'c':
+			Database_create(conn);
+			Database_write(conn);
+			break;
 
+		case 'g':
+			if(argc != 4) die("Need an id to get");
 
+			Database_get(conn, id);
+			break;
+
+		case 's':
+			if(argc != 6) die("Need id, name, email to set");
+			
+			Database_set(conn, id ,argv[4], argv[5]);
+			Database_write(conn);
+			break;
+
+		case 'd':
+			if(argc != 4) die("Need id to delete");
+
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+
+		case 'l':
+			Database_list(conn);
+			break;
+
+		default:
+			die("Invalid action, only: c=create, g=get, s=set, d=del, l=list");
+	}
+
+	Database_close(conn);
+
+	return 0;
+}

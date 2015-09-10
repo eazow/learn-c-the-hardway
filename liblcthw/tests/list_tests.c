0710d1c564e9a9712de9b50fc6a2dbf9b4353d8a
hhmhywzr <eazow@163.com>
2015-09-10 11:27:28 +0800

    update liblcthw

--- liblcthw/tests/list_tests.c
+++ liblcthw/tests/list_tests.c
@@ -15,10 +15,44 @@ char *test_create()
 	return NULL;
 }
 
+char *test_destroy()
+{
+	list_clear_destroy(list);
+
+	return NULL;
+}
+
+char *test_push_pop()
+{
+	list_push(list, test1);
+	mu_assert(list_last(list)==test1, "Wrong last value");
+
+	list_push(list, test2);
+	mu_assert(list_last(list)==test2, "Wrong last value");
+
+	list_push(list, test3);
+	mu_assert(list_last(list)==test3, "Wrong last value");
+	mu_assert(list_count(list)==3, "Wrong count on push");
+
+	char *value = list_pop(list);
+	mu_assert(value==test3, "Wrong value on pop");
+
+	value = list_pop(list);
+	mu_assert(value==test2, "Wrong value on pop");
+
+	value = list_pop(list);
+	mu_assert(value==test1, "Wrong value on pop");
+	mu_assert(list_count(list)==0, "Wrong count after pop");
+
+	return NULL;
+}
+
 char *all_tests() {
 	mu_suite_start();
 
 	mu_run_test(test_create);
+	mu_run_test(test_push_pop);
+	mu_run_test(test_destroy);
 
 	return NULL;
 }

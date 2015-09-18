c6b20de7cf34b33e4aec666070641b3b86950ebb
hhmhywzr <eazow@163.com>
2015-09-18 17:29:08 +0800

    add darray.h

---
+++ liblcthw/src/lcthw/darray.h
@@ -0,0 +1,30 @@
+#ifndef _DArray_h
+#define _DArray_h
+
+#include <stdlib.h>
+#include <assert.h>
+#include <lcthw/dbg.h>
+
+typedef struct DArray {
+	int end;
+	int max;
+	size_t element_size;
+	size_t expand_size;
+	void **contents;
+} DArray;
+
+DArray *darray_create(size_t element_size, size_t initial_max);
+
+void darray_destroy(DArray *array);
+
+void darray_clear(DArray *array);
+
+int darray_expand(DArray *array);
+
+int darray_contract(DArray *array);
+
+int darray_push(DArray *array, void *element);
+
+void *darray_pop(DArray *array);
+
+void darray_clear_destroy(DArray *array);

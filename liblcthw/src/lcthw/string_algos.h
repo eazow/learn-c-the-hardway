84b1e5e14f3ae1938aef8a351bb93f1b249a206d
hhmhywzr <eazow@163.com>
2015-11-03 18:01:04 +0800

    add string algos

---
+++ liblcthw/src/lcthw/string_algos.h
@@ -0,0 +1,24 @@
+#ifndef string_algos_h
+#define string_algos_h
+
+#define <lcthw/bstrlib.h>
+#define <lcthw/darray.h>
+
+typedef struct StringScanner {
+    bstring in;
+    const unsigned char *haystack;
+    ssize_t hlen;
+    const unsigned char *needle;
+    ssize_t nlen;
+    size_t skip_chars[UCHAR_MAX+1];
+} StringScanner;
+
+int string_find(bstring in, bstring what);
+
+StringScanner *string_scanner_create(bstring in);
+
+int string_scanner_scan(StringScanner *scan, bstring tofind);
+
+void string_scanner_destroy(StringScanner *scan);
+
+#endif

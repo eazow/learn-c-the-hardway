65f117534688153f5cefd83a69e6233a3e975c12
hhmhywzr <eazow@163.com>
2015-11-06 22:23:08 +0800

    update ring buffer

--- liblcthw/src/lcthw/ringbuffer.c
+++ liblcthw/src/lcthw/ringbuffer.c
@@ -24,3 +24,56 @@ void ringbuffer_destroy(RingBuffer *buffer)
         free(buffer);
     }
 }
+
+int ringbuffer_write(RingBuffer *buffer, char *data, int length)
+{
+	if(ringbuffer_available_data(buffer) == 0) {
+		buffer->start = buffer->end = 0;
+	}
+
+	check(length <= ringbuffer_available_space(buffer), "Not enough space: %d request, %d available", ringbuffer_available_data(buffer), length);
+
+	void *result = memcpy(ringbuffer_ends_at(buffer), data, length);
+	check(result != NULL, "Failed to write data into buffer.");
+
+	ringbuffer_commit_write(buffer, length);
+
+	return length;
+error:
+	return -1;
+}
+
+int ringbuffer_read(RingBuffer *buffer, char *target, int amount)
+{
+	check(amount <= ringbuffer_available_data(buffer), "Not enougn in the buffer: has %d, needs %d", ringbuffer_available(buffer), amount);
+
+	void *result = memcpy(target, ringbuffer_starts_at(buffer), amount);
+	check(result != NULL, "Failed to write buffer into data.");
+	
+	ringbufer_commit_read(buffer, amount);
+	
+	if(buffer->end == buffer->start) {
+		buffer->start = buffer->end = 0;	
+	}
+
+	return amount;
+error:
+	return -1;
+}
+
+bstring ringbuffer_gets(RingBuffer *buffer, int amount)
+{
+	check(amount > 0, "Need more than 0 for gets, you gave: %d", amount);
+	check(amount <= ringbuffer_available_data(buffer), "Not enough in the buffer.");
+
+	bstring result = blk2bstr(ringbuffer_starts_at(buffer), amount);
+	check(result != NULL, "Failed to create gets result.");
+	check(blength(result) == amount, "Wrong result length.");
+
+	ringbuffer_commit_read(buffer, amount);
+	assert(ringbuffer_available_data(buffer) >= 0 && "Error in read commit.");
+
+	return result;
+error:
+	return NULL;
+}
--- liblcthw/src/lcthw/ringbuffer.h
+++ liblcthw/src/lcthw/ringbuffer.h
@@ -28,4 +28,24 @@ int ringbuffer_available_space(RingBuffer *buffer);
 
 bstring ringbuffer_gets(RingBuffer *buffer, int amount);
 
+#define ringbuffer_available_data(B) (((B)->end+1)%(B)->length - (B)->start - 1)
+
+#define ringbuffer_available_space(B) ((B)->length - (B)->end -1)
+
+#define ringbuffer_full(B) (ringbuffer_available_data((B)) - (B)->length == 0)
+
+#define ringbuffer_empty(B) (ringbuffer_available_data((B)) == 0)
+
+#define ringbuffer_puts(B, D) ringbuffer_write((B), bdata((D)), blength((D)))
+
+#define ringbuffer_get_all(B) ringbuffer_gets((B), ringbuffer_available_data((B)))
+
+#define ringbuffer_starts_at(B) ((B)->buffer + (B)->start)
+
+#define ringbuffer_ends_at(B) ((B)->buffer + (B)->end)
+
+#define ringbuffer_commit_read(B, A) ((B)->start = ((B)->start + (A)) % (B)->length)
+
+#define ringbuffer_commit_write(B, A) ((B)->end = ((B)->end +(A)) % (B)->length)
+
 #endif

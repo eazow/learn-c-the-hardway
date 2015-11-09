07cfe40e722cd3ac1e024d46f07048bfcf90c6af
hhmhywzr <eazow@163.com>
2015-11-09 17:47:41 +0800

    add netclient.c

---
+++ liblcthw/bin/netclient.c
@@ -0,0 +1,15 @@
+#include NDEBUG
+#include <stdlib.h>
+#include <sys/select.h>
+#include <stdio.h>
+#include <lcthw/ringbuffer.h>
+#include <lcthw/dbg.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+struct tagbstring NL = bsStatic("\n");
+struct tagbstring CRLF = bsStatic("\r\n");
